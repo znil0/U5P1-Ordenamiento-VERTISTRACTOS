10c3d5a7ed47717431185654393c4606aa8304a9
znil0 <nilbisclavret@gmail.com>
2025-08-28 10:44:28 -0600

    First Commit

---
+++ src/u5p1_ordenamiento/U5P1_Ordenamiento.java
@@ -0,0 +1,28 @@
+// PRINCIPAL
+
+
+package u5p1_ordenamiento;
+
+/**
+ * UNIDAD 5 - PRÁCTICA 1
+ * SISTEMA DE ORDENAMIENTO
+ * 
+ *
+ * INTEGRANTES DEL EQUIPO:
+ * + Rojano Meza Leonardo Gael
+ * + Mártir Partida José Manuel
+ * + Lozano Mora Juan Pablo
+ * + Ibarra Arellano Imanol
+ * + Zavala Gil Lourdes Julíriz
+ */
+
+
+public class U5P1_Ordenamiento {
+
+    public static void main(String[] args) {
+        // Se ejecuta el form y poco más.
+        PrincipalForm pf = new PrincipalForm();
+        pf.setVisible(true);
+    }
+    
+}
